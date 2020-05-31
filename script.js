a9cbfc8d6d20dc59e438b0a84fe7ed48317bcfb9
Héctor Espinoza <hespinoza@uchile.cl>
2020-05-31 19:18:38 -0400

    volume setting implemented

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const nextBtn = document.getElementById('next');
 const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
+const timestamp = document.getElementById('timestamp');
+const durationstamp = document.getElementById('duration');
+const volume = document.getElementById('volume');
+
 const title = document.getElementById('title');
 
 
@@ -35,6 +39,12 @@ function restartSong() {
     audio.currentTime = 0;
     playSong();
 }
+function stopSong() {
+    audio.pause();
+    audio.currentTime = 0;
+    musicContainer.classList.remove('play');
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+}
 
 // Update progress bar
 function updateProgress(e) {
@@ -54,8 +64,6 @@ function updateProgress(e) {
     }
     timestamp.innerText = `${mins}:${secs}`;
 
-
-
 }
 // Set progress bar 
 function setProgress(e) {
@@ -65,7 +73,13 @@ function setProgress(e) {
     audio.currentTime = (clickX / width) * duration;
 }
 
+function setVolume() {
+    audio.volume = volume.value / 100;
 
+}
+
+
+// Event Listeners
 playBtn.addEventListener('click', () => {
     const isPlaying = musicContainer.classList.contains('play');
 
@@ -77,11 +91,31 @@ playBtn.addEventListener('click', () => {
     }
 })
 
+// This avoids audio duration to be NaN
+audio.addEventListener('loadedmetadata', () => {
+    let mins = Math.floor(audio.duration / 60);
+    if (mins < 10) {
+        mins = String(mins);
+    }
+    let secs = Math.floor(audio.duration % 60);
+    if (secs < 10) {
+        secs = '0' + String(secs);
+    }
+    durationstamp.innerText = `${mins}:${secs}`;
+});
+
+
 // Update song time
 audio.addEventListener('timeupdate', updateProgress);
 
 // Click on progress bar
 progressContainer.addEventListener('click', setProgress);
 
-// previous botton restarts the song 
+// previous and next botton restart the song (for now)
 prevBtn.addEventListener('click', restartSong);
+nextBtn.addEventListener('click', restartSong);
+
+// after song is finished 
+audio.addEventListener('ended', stopSong);
+
+volume.addEventListener('change', setVolume);
